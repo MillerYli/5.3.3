8112db776c9b7f2fb2dff249adf97e289db867b5
Yulia Miller <miller82.98@mail.ru>
2022-12-28 23:45:47 -0800

    Добавьте файлы проекта.

---
+++ 5.3.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myage = 24;
+            int data = 10;
+            var arr = new int[] {3, 6, 8};
+            Console.WriteLine("Мой возраст: " + myage);
+
+            ChangeAge(ref myage);       //Добавлено служебное слово ref
+
+            Console.WriteLine("Мой измененный возраст: " + myage);
+
+            BigDataOperation(arr, ref data);
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(data);
+
+        }
+
+        static void ChangeAge(ref int age)      //Добавлено служебное слово ref(можно изменять данные)
+        {
+            Console.Write("Введите свой возраст: ");
+
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+        static void BigDataOperation(in int[] arr, ref int data)
+        {
+            data = 6;
+            arr[0] = 5;
+        }
+    }
+}
